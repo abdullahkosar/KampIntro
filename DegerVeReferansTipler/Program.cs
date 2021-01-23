3f1f2a623ffc5f62df4e17a62adb8077ae973f7f
Abdullah KOŞAR <abdullahkosar8538@gmail.com>
2021-01-23 19:20:53 +0300

    16

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -15,13 +15,14 @@ namespace DegerVeReferansTipler
             sayi2 = 65;
 
             //array - diziler referans tiplerdir 
+            
 
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
-            sayilar2[0] = 999;
-
             //bu aşamada sayilar2 dizisinin 0. elemanı 999 yapılıyor ve bu sayilar1 dizisine aktarılıyor referansı sayilar2 çünkü
+            sayilar2[0] = 999;
+            
             Console.WriteLine(sayilar1[0]);
 
                
